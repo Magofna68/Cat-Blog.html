85b7d35ffd627d57fdac75e3e3a58f5b43522903
Brandon-Magofna <magofna68@gmail.com>
2021-03-23 16:23:57 -0700

    add more notes
    co-authored-by: Jeremy Banka <JeremyBanka@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-// first $: is the document ready?
+// first $: is the document ready? 
+// If it is, the contents of the function() can run
 $(function() {
   // second $: when we "click" #lightswitch, run the following function()
   $("#lightswitch").on("click", function() { 
@@ -6,3 +7,26 @@ $(function() {
     $("body").toggleClass("darkmode")
   })
 })
+
+
+function sub(a, b) { 
+  return a - b 
+}
+
+const add = (a, b) => {
+  return a + b
+}
+
+const mul = (a, b) => a * b // auto return!
+
+const foodReviews = { // this is an Object with 2 properties
+  boiledEgg: 'overcooked',
+  banana: 'perfectly ripe but delicious',
+  any: food => food + ' was pretty good!'
+}
+
+// foodReviews.boiledEgg -> 'overcooked'
+// foodReviews.any(orange) -> 'orange was pretty good!'
+
+
+console.log(add(2, 2))
